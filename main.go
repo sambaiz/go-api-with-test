93f4564c8c4bd9053026b7881f4bfd989d242306
sambaiz <godgourd@gmail.com>
2016-10-24 19:38:56 +0900

    fix handler func

--- main.go
+++ main.go
@@ -1,43 +1,47 @@
 package main
 
 import (
-    "net/http"
-    "github.com/labstack/echo"
-    "github.com/labstack/echo/engine/standard"
-    "github.com/sambaiz/go-api-with-test/handler"
-    "github.com/gocraft/dbr"
-    "github.com/facebookgo/grace/gracehttp"
-    "github.com/labstack/echo/middleware"
-    _ "github.com/go-sql-driver/mysql"
-    "github.com/sambaiz/go-api-with-test/conf"
+	"github.com/facebookgo/grace/gracehttp"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gocraft/dbr"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+	"github.com/labstack/echo/middleware"
+	"github.com/sambaiz/go-api-with-test/conf"
+	"github.com/sambaiz/go-api-with-test/handler"
+	"net/http"
 )
 
 func main() {
-    conf, _ := conf.Parse()
-    conn, err := dbr.Open("mysql", conf.Db, nil)
-    if err != nil {
-        panic(err)
-    }
-    conn.SetMaxIdleConns(200)
-    conn.SetMaxOpenConns(200)
+	conf, _ := conf.Parse()
+	conn, err := dbr.Open("mysql", conf.Db, nil)
+	if err != nil {
+		panic(err)
+	}
+	conn.SetMaxIdleConns(200)
+	conn.SetMaxOpenConns(200)
 
-    e := echo.New()
+	e := echo.New()
 
-    // middlewares
-    e.Use(middleware.Logger())
-    e.Use(middleware.Recover())
-    e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-        AllowOrigins: []string{"*"},
-        AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
-    }))
+	// middlewares
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+	}))
 
-    // endpoints
-    e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Hello, World!")
-    })
-    e.GET("/messages", handler.NewMessageWithSession(conn.NewSession(nil)).GetMessages)
-    e.POST("/messages", handler.NewMessageWithSession(conn.NewSession(nil)).CreateMessage)
-    std := standard.New(":1323")
-    std.SetHandler(e)
-    gracehttp.Serve(std.Server)
+	// endpoints
+  e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.GET("/messages", func(c echo.Context) error {
+		return handler.NewMessageWithSession(conn.NewSession(nil)).GetMessages(c)
+	})
+	e.POST("/messages", func(c echo.Context) error {
+		return handler.NewMessageWithSession(conn.NewSession(nil)).CreateMessage(c)
+	})
+	std := standard.New(":1323")
+	std.SetHandler(e)
+	gracehttp.Serve(std.Server)
 }
